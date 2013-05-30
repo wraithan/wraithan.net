9eba41b8b299c31c8cb11a1fc7bc46d249e569e3
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-05-30 15:52:18 -0700

    try out disqus again

--- publishconf.py
+++ publishconf.py
@@ -17,5 +17,5 @@ DELETE_OUTPUT_DIRECTORY = True
 # Uncomment following line for absolute URLs in production:
 RELATIVE_URLS = False
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "wraithannet"
 #GOOGLE_ANALYTICS = ""
